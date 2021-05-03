e98ca0fcf982df513b26ed9849f1f6bcc41f232e
Nihar Kashyap <nihar1kashyap7@gmail.com>
2021-05-03 08:25:31 +0530

    DFD

---
+++ e_news/lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TU E News'),
+        ),
+        body: Center(
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              Container(
+                height: 50,
+                color: Colors.amber[600],
+                child: const Center(child: Text('Entry A')),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[500],
+                child: const Center(child: Text('Entry B')),
+              ),
+              Container(
+                height: 50,
+                color: Colors.amber[100],
+                child: const Center(child: Text('Entry C')),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
