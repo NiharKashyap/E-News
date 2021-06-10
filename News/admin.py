c1fd53029da583b6588fb8b9c81fd670870a6fa9
Nihar Kashyap <nihar1kashyap7@gmail.com>
2021-06-10 15:14:09 +0530

    Admin shows news title now

--- News/admin.py
+++ News/admin.py
@@ -5,8 +5,12 @@ from .models import News, BookMarks
 
 class NewsAdmin(admin.ModelAdmin):
     fields = ['title','body', 'posted']
+    list_display = ['title', 'posted']
+
+
 class BookMarksAdmin(admin.ModelAdmin):
     fields = ['user', 'news']
+    list_display = ['user', 'news']
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(BookMarks, BookMarksAdmin)
